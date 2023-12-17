4d18c438221db9492586d2b9ec8b1691da25204b
Maximo Brunetti <maxobrunetti@gmail.com>
2023-12-17 01:20:37 -0300

    Stylized

---
+++ Curriculum/script.js
@@ -0,0 +1,24 @@
+// Cambiar el color de fondo al pasar el mouse sobre los enlaces
+const links = document.querySelectorAll('a');
+
+links.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.style.backgroundColor = '#ecf0f1';
+    });
+
+    link.addEventListener('mouseout', () => {
+        link.style.backgroundColor = 'transparent';
+    });
+});
+
+// Agregar un efecto de resaltado al hacer clic en los títulos
+const titles = document.querySelectorAll('h1, h2, h3');
+
+titles.forEach(title => {
+    title.addEventListener('click', () => {
+        title.style.color = '#e74c3c';
+        setTimeout(() => {
+            title.style.color = ''; // Restablecer el color original después de 500 ms
+        }, 500);
+    });
+});
