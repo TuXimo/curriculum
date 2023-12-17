6a55b41fb889b8021c8a545749e2d8febd5a1c11
Maximo Brunetti <maxobrunetti@gmail.com>
2023-12-17 01:38:48 -0300

    Stylized part 3

--- Curriculum/script.js
+++ Curriculum/script.js
@@ -1,24 +1,26 @@
-// Cambiar el color de fondo al pasar el mouse sobre los enlaces
-const links = document.querySelectorAll('a');
+document.addEventListener("DOMContentLoaded", function() {
+    // Cambiar el color de fondo al pasar el mouse sobre los enlaces
+    const links = document.querySelectorAll('a');
 
-links.forEach(link => {
-    link.addEventListener('mouseover', () => {
-        link.style.backgroundColor = '#ecf0f1';
+    links.forEach(link => {
+        link.addEventListener('mouseenter', () => {
+            link.style.backgroundColor = '#ecf0f1';
+        });
+
+        link.addEventListener('mouseleave', () => {
+            link.style.backgroundColor = 'transparent';
+        });
     });
 
-    link.addEventListener('mouseout', () => {
-        link.style.backgroundColor = 'transparent';
-    });
-});
+    // Agregar un efecto de resaltado al hacer clic en los títulos
+    const titles = document.querySelectorAll('h1, h2, h3');
 
-// Agregar un efecto de resaltado al hacer clic en los títulos
-const titles = document.querySelectorAll('h1, h2, h3');
-
-titles.forEach(title => {
-    title.addEventListener('click', () => {
-        title.style.color = '#e74c3c';
-        setTimeout(() => {
-            title.style.color = ''; // Restablecer el color original después de 500 ms
-        }, 500);
+    titles.forEach(title => {
+        title.addEventListener('click', () => {
+            title.style.color = '#e74c3c';
+            setTimeout(() => {
+                title.style.color = ''; // Restablecer el color original después de 500 ms
+            }, 500);
+        });
     });
 });
