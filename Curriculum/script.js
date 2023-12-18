8fd99eb5ba7a6cf67a71dc0256e141d3e66c8cc9
Maximo Brunetti <maxobrunetti@gmail.com>
2023-12-18 17:16:30 -0300

    stylized part 7

--- Curriculum/script.js
+++ Curriculum/script.js
@@ -2,25 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     // Cambiar el color de fondo al pasar el mouse sobre los enlaces
     const links = document.querySelectorAll('a');
 
+
     links.forEach(link => {
+        let originalColor = link.style.color;
+
         link.addEventListener('mouseenter', () => {
-            link.style.backgroundColor = '#ecf0f1';
+            link.style.color = '#ffffff';
         });
 
         link.addEventListener('mouseleave', () => {
-            link.style.backgroundColor = 'transparent';
-        });
-    });
-
-    // Agregar un efecto de resaltado al hacer clic en los títulos
-    const titles = document.querySelectorAll('h1, h2, h3');
-
-    titles.forEach(title => {
-        title.addEventListener('click', () => {
-            title.style.color = '#e74c3c';
-            setTimeout(() => {
-                title.style.color = ''; // Restablecer el color original después de 500 ms
-            }, 500);
+            link.style.color = originalColor;
         });
     });
 });
